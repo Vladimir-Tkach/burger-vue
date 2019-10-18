7262b397fa3f3a106d57204a206df284dab7fdce
Vladimir Tkachenko <vladimir.tkachenko@progforce.com>
2019-10-18 16:02:26 +0300

    1 commit

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,31 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+//import testComponent from './components/testComponent.vue'
+
 
 export default {
   name: 'app',
+
+  data: function(){
+    return{
+      text: 'test'
+    }
+  },
+
   components: {
-    HelloWorld
-  }
+
+  },
+
+  methods: {
+
+  },
+  
+
 }
 </script>
 
